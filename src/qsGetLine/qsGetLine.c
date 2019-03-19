39d8530ebe44756d96d35c4865579f55ee535b4a
Steven <steven.m.hawes@wmich.edu>
2019-03-19 13:38:11 -0400

    Started Input

--- src/qsGetLine/qsGetLine.c
+++ src/qsGetLine/qsGetLine.c
@@ -1,5 +1,20 @@
 #include "qsGetLine.h"
 
 int qsGetline(char* line, int nline) {
+    printf("Please input the values A, B, and C seperated by spaces\n");
+    printf("input A B C : <A> <B> <C>\n");
     
+    double a;
+    double b;
+    double c;
+    int ret;
+
+    char temp[nline];
+    fgets(temp, nline - 1, stdin);
+    if((ret = sscanf(temp, "%lf %lf %lf", &a, &b, &c)) != 3) {
+        printf("Bad Input (Please update this to reasons why its bad)");
+        return 0;
+    }
+
+    return 1;
 }
