4cfd6385fe3c7c2e56fe2f695975ea7a38fbbc8a
steven <stevenmhawes@gmail.com>
2019-03-24 18:10:54 -0500

    Log changes

--- src/qsLog/qsLog.c
+++ src/qsLog/qsLog.c
@@ -1,8 +1,16 @@
 #include "qsLog.h"
 
 int qsLog(char *line) {
+
     File *logFile;
+
+    // Will check for the file and append to it.
+    // If file is not available it will create a new file.
     fd = fopen("/qsLog/logFile.txt", "a");
-    fprintf(fp, line);
+
+    // Prints the char array
+    fprintf(fd, "%s\n", line);
+
+    //Closes the file
     fclose(fd);
 }
