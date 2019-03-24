9b488753a3c7a61b215605d0fcd31249c93474b2
edgarEC45 <47834428+edgarEC45@users.noreply.github.com>
2019-03-24 19:42:01 -0400

    Update qsErrors.c
    Added an error case for when results x1 or x2 are not within the 32-bit float range.

--- src/qsErrors/qsErrors.c
+++ src/qsErrors/qsErrors.c
@@ -25,6 +25,12 @@ int qsErrors(int error, char* line, int len) {
         case PRODUCT:
             strncpy(line, "The product of entered coefficients is out of bounds of a 64-bit double", len);
             break;
+        case X1_OUT:
+            strncpy(line, "The value for x1 is out of the 32-bit float range", len);
+            break;
+        case X2_OUT:
+            strncpy(line, "The value for x2 is out of the 32-bit float range", len);
+            break;
     }
     
     return error;
