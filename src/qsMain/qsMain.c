55ab57a44e8819e9eebfee4d342c2acbde5d52b5
steven <stevenmhawes@gmail.com>
2019-03-24 17:44:46 -0500

    main Changes

--- src/qsMain/qsMain.c
+++ src/qsMain/qsMain.c
@@ -18,47 +18,49 @@ int main(int argc, char** argv) {
     double c  = 0;
     double x1 = 0;
     double x2 = 0;
-    
+
     // Check if logging enable parameter passed as command line argument
 //    if((argc > 1) && (atoi(argv[1]) == 1)) {
 //        qsLogEnable();
 //    }
-    
+
     // Print program information header
+    printf("Quadratic Solver\n");
+    printf("Version: 0.1.0\n");
     // ...
-    
+
     // Get input line
     if((ret = qsGetLine(line, nline)) != OK) {
         qsErrors(ret, error, nline);
         return ret;
     }
-    
+
     // Check if user prompted for help
 //    qsHelp();
-    
+
     // Validate input
     if((ret = qsValidate(line, nline, &a, &b, &c)) != OK) {
         qsErrors(ret, error, nline);
         return ret;
     }
-    
+
     // Apply quad solver
     if((ret = qsSolve(a, b, c, &x1, &x2)) > ROOT_2) {
         qsErrors(ret, error, nline);
         return ret;
     }
-    
+
     // Check results
 //    if((ret = qsResults()) != OK) {
 //        qsErrors(ret, error, nline);
 //        return ret;
 //    }
-    
+
     // Output results
     if((ret = qsPutLine(line, strnlen(line, nline))) != OK) {
         qsErrors(ret, error, nline);
         return ret;
     }
-    
+
     return OK;
 }
