7d27c33b4a8861d6027697dc4437a5897c12bbcf
Steven <steven.m.hawes@wmich.edu>
2019-03-24 17:46:01 -0400

    Update qsGetLine.c
    getLine changes

--- src/qsGetLine/qsGetLine.c
+++ src/qsGetLine/qsGetLine.c
@@ -1,18 +1,18 @@
 #include "qsGetLine.h"
+#include <math.h>
 
 int qsGetline(char* line, int nline) {
     printf("Please input the values A, B, and C seperated by spaces\n");
     printf("input A B C : <A> <B> <C>\n");
     
-    double a;
-    double b;
-    double c;
+    char *a;
+    char *b;
+    char *c;
     int ret;
 
     char temp[nline];
     fgets(temp, nline - 1, stdin);
-    if((ret = sscanf(temp, "%lf %lf %lf", &a, &b, &c)) != 3) {
-        printf("Bad Input (Please update this to reasons why its bad)");
+    if((ret = sscanf(temp, "%s %s %s", &a, &b, &c)) != 3) {
         if (ret <= 2) {
             printf("Input has less than the required input values (Required: 3)");
             return ARGS;
