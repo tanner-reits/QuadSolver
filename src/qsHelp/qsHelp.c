23f082d0eee5a779e0a9932207590c013e164553
steven <stevenmhawes@gmail.com>
2019-03-24 17:31:43 -0500

    Basic changes

--- src/qsHelp/qsHelp.c
+++ src/qsHelp/qsHelp.c
@@ -1,5 +1,7 @@
 #include "qsHelp.h"
 
-int qsHelp(...) {
-    
+int qsHelp() {
+    printf("Welcome to the Quad Solver Help Page\n");
+    printf("This Quad Solver program retieves input values of A, B, and C from the user.\n");
+    printf("Example:\n3 4 9\nReturns:\n");
 }
