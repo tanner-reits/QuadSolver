ed64550a8094c734c7ef4dc0de0f61e91f62f263
edgarEC45 <47834428+edgarEC45@users.noreply.github.com>
2019-03-25 16:06:20 -0400

    Took out the float check and added output

--- src/qsResults/qsResults.c
+++ src/qsResults/qsResults.c
@@ -1,16 +1,7 @@
 #include "qsResults.h"
 
 int qsResults(double *x1, double *x2) {
-    //make sure x1 is within the float range
-  if((*x1 < FLT_MIN) || (*x1 > FLT_MAX)){
-    printf("x1 out of float range\n");
-    return X1_OUTF;
-  }
-  //make sure x2 is within the float range
-  if((*x2 < FLT_MIN) || (*x2 > FLT_MAX)){
-    printf("x2 out of float range\n");
-    return X2_OUTF;
-  }
+    
   //make sure x1 is within the double range
   if((*x1 < DBL_MIN) || (*x1 > DBL_MAX)){
     printf("x1 out of double range\n");
@@ -22,6 +13,8 @@ int qsResults(double *x1, double *x2) {
     printf("x1 out of double range\n");
     return X2_OUTD;
   }
+    
+    printf("Results: x1 = %f, x2 = %f\n", *x1, *x2);
 
   return OK;
 }
