e8e615a818b0ab0f40b0866fede85e8c3927d346
edgarEC45 <47834428+edgarEC45@users.noreply.github.com>
2019-03-24 20:56:55 -0400

    update qsResults.c
    Added code for the function, reflecting the definition update

--- src/qsResults/qsResults.c
+++ src/qsResults/qsResults.c
@@ -1,5 +1,27 @@
 #include "qsResults.h"
 
-int qsResults(...) {
-    
+int qsResults(double *x1, double *x2) {
+    //make sure x1 is within the float range
+  if((*x1 < FLT_MIN) || (*x1 > FLT_MAX)){
+    printf("x1 out of float range\n");
+    return X1_OUTF;
+  }
+  //make sure x2 is within the float range
+  if((*x2 < FLT_MIN) || (*x2 > FLT_MAX)){
+    printf("x2 out of float range\n");
+    return X2_OUTF;
+  }
+  //make sure x1 is within the double range
+  if((*x1 < DBL_MIN) || (*x1 > DBL_MAX)){
+    printf("x1 out of double range\n");
+    return X1_OUTD;
+  }
+
+  //make sure x2 is within the double range
+  if((*x2 < DBL_MIN) || (*x2 > DBL_MAX)){
+    printf("x1 out of double range\n");
+    return X2_OUTD;
+  }
+
+  return OK;
 }
