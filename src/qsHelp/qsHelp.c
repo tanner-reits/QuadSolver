21a96f81011d3bc0f6be034afa40c7e3d66e2e2b
edgarEC45 <47834428+edgarEC45@users.noreply.github.com>
2019-03-26 15:04:06 -0400

    Update qsHelp.c

--- src/qsHelp/qsHelp.c
+++ src/qsHelp/qsHelp.c
@@ -3,8 +3,8 @@
 int qsHelp() {
     printf("Welcome to the Quad Solver Help Page\n");
     printf("This Quad Solver program retieves input values of A, B, and C from the user.\n");
-    printf("Example:\n1 3 2\nReturns: x1 = -2, x2 = -1\n\n");
-    printf("Example:\n10 20 0\nReturns: x1 = -2, x2 = 0\n\n");
-    printf("Example:\n10 20 0\nReturns: x1 = -2, x2 = 0\n\n");
-    printf("Example:\n1 2 1\nReturns: x1 = -1, x2 = -1\n\n");
+    printf("Example input:\n1 3 2\nReturns: x1 = -2, x2 = -1\n\n");
+    printf("10 20 0\nReturns: x1 = -2, x2 = 0\n\n");
+    printf("10 20 0\nReturns: x1 = -2, x2 = 0\n\n");
+    printf("1 2 1\nReturns: x1 = -1, x2 = -1\n\n");
 }
