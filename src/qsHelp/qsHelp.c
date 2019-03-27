7a6a138dfda153cef2c8c43f1f76b15ad5d07cf9
Tanner Reits <Tanner@Tanners-MBP.attlocal.net>
2019-03-26 22:06:53 -0400

    Format changes to help menu

--- src/qsHelp/qsHelp.c
+++ src/qsHelp/qsHelp.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 
 int qsHelp() {
+    printf("*******************************************************************************\n");
+    printf("Welcome to the Quad Solver Help Menu!\n");
+    
     // Explanation
-    printf("\nWelcome to the Quad Solver Help Page\n");
-    printf("This Quad Solver program retrieves input values of A, B, and C from the user.\n");
-    printf("All input values must be within the range of a 32-bit single precision float\n")
+    printf("ABOUT:\n");
+    printf("\tThis Quad Solver program retrieves input values of A, B, and C from the user.\n");
+    
+    // Input
+    printf("INPUT:\n");
+    printf("\tAll input values must be within the range of a 32-bit single precision float\n");
     
     // Examples of input format and output
-    printf("Example input and output:\n");
+    printf("EXAMPLE INPUT AND OUTPUT:\n");
     printf("\t1 3 2\n\tReturns: x1 = -2, x2 = -1\n\n");
     printf("\t10 20 0\n\tReturns: x1 = -2, x2 = 0\n\n");
-    printf("\t1 2 1\n\tReturns: x1 = -1, x2 = -1\n\n");
+    printf("\t1 2 1\n\tReturns: x1 = -1\n");
     
     // Logging
+    printf("LOGGING:\n");
+    
+    printf("*******************************************************************************\n\n");
     
     return 1;
 }
