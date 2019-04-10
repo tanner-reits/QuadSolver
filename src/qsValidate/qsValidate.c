46f92699f026fa44138d25f5ed5fc27c98565386
Audre <audrewow@gmail.com>
2019-04-09 23:25:24 -0400

    Add check for decimal digits greater than 7 with input.

--- src/qsValidate/qsValidate.c
+++ src/qsValidate/qsValidate.c
@@ -7,25 +7,40 @@
 #include "../qsErrors/qsErrors.h"
 
 
-/* 
+/*
 */
 int qsValidate(char* line, int nline, double* a, double* b, double* c) {
     // Initialize pointers
     *a = NAN;
     *b = NAN;
     *c = NAN;
-    
+
     // Variables for parsing
     char* err  = NULL;
     char* tok  = NULL;
     int args   = 0;
     int format = 0;
     char buf[nline + 1];
-    
+
+    // Variables to check number of decimal digits
+    char *ptr = NULL;
+    int point = '.';
+    int precisionLoss = 0;
+
     strncpy(buf, line, nline);
-    
+
     // Parse string into a, b, and c coefficients
     if((tok = strtok(buf, " ")) != NULL) {
+         ptr = strchr(tok, point);
+         if (ptr){
+            ++ptr;
+            size_t test = strlen(ptr);
+
+            if (test > 7){
+                 precisionLoss = 1;
+            }
+         }
+
         *a = strtod(tok, &err);
         if(*err == 0 && !isnan(*a)) {
             args++;
@@ -36,6 +51,17 @@ int qsValidate(char* line, int nline, double* a, double* b, double* c) {
         }
     }
     if((tok = strtok(NULL, " ")) != NULL) {
+         ptr = strchr(tok, point);
+         if (ptr){
+            ++ptr;
+            size_t test = strlen(ptr);
+
+            if (test > 7){
+                 precisionLoss = 1;
+            }
+         }
+
+
         *b = strtod(tok, &err);
         if(*err == 0 && !isnan(*b)) {
             args++;
@@ -46,6 +72,16 @@ int qsValidate(char* line, int nline, double* a, double* b, double* c) {
         }
     }
     if((tok = strtok(NULL, " ")) != NULL) {
+        ptr = strchr(tok, point);
+        if (ptr){
+           ++ptr;
+           size_t test = strlen(ptr);
+
+           if (test > 7){
+                precisionLoss = 1;
+           }
+        }
+
         *c = strtod(tok, &err);
         if(*err == 0 && !isnan(*c)) {
             args++;
@@ -55,17 +91,21 @@ int qsValidate(char* line, int nline, double* a, double* b, double* c) {
             format = 1;
         }
     }
-    
+
+    if (precisionLoss){
+         printf("WARNING! Possible precision loss.\n");
+    }
+
     // If formatting flag set or too few args, return corresponding error
     if(args < 3 || format) {
         return FORMAT;
     }
-    
+
     // Check input ranges
     if(fabs(*a) > FLT_MAX) {
         *a = NAN;
         return A_OUT;
-    } 
+    }
     if(fabs(*b) > FLT_MAX) {
         *b = NAN;
         return B_OUT;
@@ -73,7 +113,7 @@ int qsValidate(char* line, int nline, double* a, double* b, double* c) {
     if(fabs(*c) > FLT_MAX) {
         *c = NAN;
         return C_OUT;
-    } 
-    
+    }
+
     return OK;
 }
